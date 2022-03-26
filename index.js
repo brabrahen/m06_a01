6927be94eb8908e661a900eecad65049a96232e1
brabala <brabrahen@gmail.com>
2022-03-26 18:35:17 -0300

    incio back express

--- index.js
+++ index.js
@@ -1,56 +1,13 @@
 const express = require('express');
 const cors = require('cors')
 const port = 3001;
-
 const app = express();
+const route = require("./src/routes/paletas.routes")
+
 app.use(cors())
 
 app.use(express.json());
 
-const paletas = [
-  {
-    id: 1,
-    sabor: 'Açaí com Leite Condensado',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: '/assets/images/acai-com-leite-condensado.png',
-    preco: 10.0,
-  },
-  {
-    id: 2,
-    sabor: 'Banana com Nutella',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: '/assets/images/banana-com-nutella.png',
-    preco: 10.0,
-  },
-  {
-    id: 3,
-    sabor: 'Chocolate Belga',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: '/assets/images/chocolate-belga.png',
-    preco: 7.0,
-  },
-];
-
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-
-app.get('/paletas', (req, res) => {
-  res.send(paletas);
-});
-
-app.get('/find-paleta/(:id)?', (req, res) => {
-  const idParam = req.params.id;
-  const unicaPaleta = paletas.find((paleta) => paleta.id == idParam);
-  console.log(unicaPaleta)
-  if (unicaPaleta === undefined) {
-    res.send({ message: 'Nenhuma paleta foi encontrada' });
-  }
-  res.send(unicaPaleta);
-});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
